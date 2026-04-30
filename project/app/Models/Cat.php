1d22e5755ba6562eefea487d6bf41b608134f474
Johanna Okas <johannaokas@gmail.com>
2026-05-01 00:20:49 +0300

    add no-referrer policy for Imgur image URLs

--- project/app/Models/Cat.php
+++ project/app/Models/Cat.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cat extends Model
 {
     protected $guarded = [];
+
+    protected $appends = ['needs_no_referrer'];
+    
+    public function getNeedsNoReferrerAttribute(): bool
+    {
+        return str_contains($this->url ?? '', 'imgur.com');
+    }
 }
--- project/resources/views/cats/show.blade.php
+++ project/resources/views/cats/show.blade.php
@@ -1,6 +1,11 @@
 <x-layout title="Cats">
     <div class="mt-8">
-        <img class="h-100 w-100 object-contain object-left" src="{{ $cat->url }}" alt="Random cat" referrerpolicy="no-referrer">
+        <img
+            class="h-120 w-120 object-contain object-left"
+            src="{{ $cat->url }}"
+            alt="Random cat"
+            @if($cat->needs_no_referrer) referrerpolicy="no-referrer" @endif
+        >
         <p class="mt-6 font-semibold"><a href="/cats/random" class="font-semibold underline hover:text-pink-500">Summon a new cat.</a></p>
     </div>
 </x-layout>
