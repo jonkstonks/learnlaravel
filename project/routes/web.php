f9fe8d4ca7d5107a81efcc3a43d21fc71d50dceb
Johanna Okas <johannaokas@gmail.com>
2026-04-30 12:48:45 +0300

    Episood 8

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Idea;
 
 Route::get('/', function () {
-    $ideas = session()->get('ideas', []);
+    // $ideas = session()->get('ideas', []);
+    // $ideas = DB::table('ideas')->get(); 
+    // $ideas = Idea::all(); // eloquent model, tegime terminalis php artisan make:model käsuga
+    // $ideas = Idea::where('status', 'pending')->get();
+    $ideas = Idea::query()
+        ->when(request('status'), function ($query, $status) {
+            $query->where('status', $status);
+        })
+        ->get();
 
-    // dd($ideas); // kuvab sessioonis salvestatud ideed
 
     return view('ideas', [
         'greeting' => 'Hello',
@@ -15,16 +23,21 @@ Route::get('/', function () {
 });
 
 Route::post('/ideas', function () {
-    $idea = request('idea'); // fetching the idea
-
-    session()->push('ideas', $idea); // pushing it to the session that is named 'ideas'
+    Idea::create([
+        'description' => request('idea'),
+        // videos pani siia 'status' => 'pending', aga ma panin selle migratsioonis juba peale
+        // Eloquent lisab ise timestampid ka (pole kohaliku aja järgi tho, 4h taga praegu)
+    ]);
 
     return redirect('/'); // redirect back to home
 });
 
 // v-- sedasi asju ei tehta päriselt, aga praegu näidiseks sobib --v
+// enam ei salvesta kirjeid sessioonis ka 
 Route::get('/delete-ideas', function() {
     session()->forget('ideas'); 
     
     return redirect('/');
 });
+
+// {{ person }} jaoks voiks lasta sisestada nime ja siis seda sessioonis hoida iguess
